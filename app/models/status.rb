2256f3505fe9b48171b33e922479ed41b0b7f2a4
Constantine Antonakos <con.antonakos@gmail.com>
2013-03-03 18:41:13 -0500

    Updated design and look for authentication system

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,4 @@
 class Status < ActiveRecord::Base
-  attr_accessible :content, :name
+  attr_accessible :content, :user_id #changed the :name field to :user_id for user model
+  belongs_to :user
 end
--- db/migrate/20130303172641_add_user_id_to_statuses.rb
+++ db/migrate/20130303172641_add_user_id_to_statuses.rb
@@ -1,3 +1,5 @@
+## rails generate migrations add_user_id_to_statuses
+
 class AddUserIdToStatuses < ActiveRecord::Migration
   def change
   	add_column :statuses, :user_id, :integer
