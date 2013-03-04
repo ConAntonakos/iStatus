c53270bbb05097563ec3e54d961cc14519002713
Constantine Antonakos <con.antonakos@gmail.com>
2013-03-04 01:32:24 -0500

    Modified status validations and tests

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,9 @@
 class Status < ActiveRecord::Base
   attr_accessible :content, :user_id #changed the :name field to :user_id for user model
   belongs_to :user
+
+  validates :content, presence: true,
+  						length: { minimum: 2}
+
+  validates :user_id, presence: true
 end
