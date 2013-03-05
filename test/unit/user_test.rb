07354f0afb99825337cb36ba277e17e700307df7
Constantine Antonakos <con.antonakos@gmail.com>
2013-03-05 00:36:20 -0500

    Fixed user validation test issue

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -28,11 +28,21 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "a user should have a profile name without spaces" do
-  	user = User.new
+    user = User.new(first_name: "Constantine", last_name: "Antonakos", email: "con.antonakos2@gmail.com")
+    user.password = user.password_confirmation = 'password'
   	user.profile_name = "My Profile With Spaces"
 
   	assert !user.save
   	assert !user.errors[:profile_name].empty?
   	assert user.errors[:profile_name].include?("Must be formatted correctly.")
   end
+
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: "Constantine", last_name: "Antonakos", email: "con.antonakos2@gmail.com")
+    user.password = user.password_confirmation = 'password'
+
+    user.profile_name = 'conantonakos1'
+    assert user.valid?
+  end
+
 end
